9084c2fa0c8230d2af717dbedd49fb1c2ef4b692
GONI Guillaume <gdn3@live.fr>
2022-08-08 01:16:54 +0200

    disable eslint complait

--- src/components/tic_tac_toe/TheClock.vue
+++ src/components/tic_tac_toe/TheClock.vue
@@ -6,6 +6,8 @@ const UPDATE_INTERVAL_SECONDS = 1000;
 const date = ref(new Date());
 
 const tick = () => (date.value = new Date());
+// We don't care for the type here, so any is fine
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 let timerId: any | null = null;
 
 onMounted(() => (timerId = setInterval(tick, UPDATE_INTERVAL_SECONDS)));
